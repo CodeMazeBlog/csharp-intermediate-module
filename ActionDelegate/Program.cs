0b247a4f1ed8d6ae91e8b550c13cfcfa43de054b
unknown <ahmad.sadegh@gmail.com>
2022-07-27 23:22:47 +0430

    action and Func example

---
+++ ActionDelegate/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ActionDelegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string, string> func = Display;
+            func("Ahmad", "Sadegh");
+            Console.ReadLine();           
+        }
+        static void Display(string firstName, string lastName)
+        {
+            string fullName = firstName + " " + lastName;
+            Console.WriteLine($"{fullName}");
+        }
+        
+    }
+}
+
---
+++ FuncDelegate/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FuncDelegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, string, string> func = Display;
+            string fullName = func("Ahmad", "Sadegh");
+            Console.WriteLine(fullName);
+            Console.ReadLine();
+        }
+        static string Display(string firstName, string lastName)
+        {
+            return firstName + ' ' + lastName;
+        }
+    }
+}
